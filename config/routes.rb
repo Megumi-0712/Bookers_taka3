092c4155ae5c4a494c2613ace893465e2a5526bf
Megumi-0712 <toruansya@yahoo.co.jp>
2020-05-31 06:29:21 +0900

    refile追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :posts
+  root to: "top#index"
+  get "home/about" => "top#about"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users
 end
