4e95261199ae8c2765cbe2c606fd38485a34f6f1
Megumi-0712 <toruansya@yahoo.co.jp>
2020-06-15 14:14:15 +0900

    spec導入

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+
+    before_action :authenticate_user!
+    before_action :signed_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+
+    def index
+        @users = User.all
+    end
+
+	def show
+        @user = User.find(params[:id])
+        @books = @user.books
+        @book = Book.new
+    end
+
+    def create
+        @user = User.find(paranms[:id])
+        if user user.authenticate(params[:session][:password])
+            flash[:notice] = "Welcome! You have signed up successfully."
+            redirect_to use_path(@user.id)
+        else
+            flash[:error]
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+    	@user = User.find(params[:id])
+    	if @user.update(user_params)
+            flash[:notice] = "You have updated user successfully."
+            redirect_to use_path(@user.id)
+        else
+            render "new"
+        end
+    end
+
+    private
+    def user_params
+    	params.require(:user).permit(:name, :profile_image, :introduction)
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)
+    end
+end
